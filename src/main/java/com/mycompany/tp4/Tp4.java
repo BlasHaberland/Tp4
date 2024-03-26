432ab934f898226981f2e2008132234266db5824
Blas Haberland <haberlandblas@gmail.com>
2024-03-26 16:21:23 -0300

    first commit

---
+++ src/main/java/com/mycompany/tp4/Tp4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tp4;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tp4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
